b157a14eb15d59ad433fcb97ba4534d02dc5c6b2
Semia Guesmi <semiaguesmi@sguesmi.cs.ucl.ac.uk>
2026-08-06 13:48:25 +0100

    solve the path of saveTrace and having trce of attack_call_withdraw traces

--- scripts/Single_function_reentrancy/attack.js
+++ scripts/Single_function_reentrancy/attack.js
@@ -2,10 +2,10 @@ const {ethers} = require("hardhat");
 const {saveTrace} = require("../../utils/saveTrace.js");
 const {loadContext} = require ("../Single_function_reentrancy/context.js");
 async function main () {
-    const ctx = await loadContext();
-    console.log("attacker account balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attackerAddress)));;
-    console.log("reentrancyTest account balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.reentrancyTestAddress)));;
-    console.log("attacker account balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attacker)));;
+  const ctx = await loadContext();
+    console.log("attacker Contract balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attackerAddress)), "ETH");;
+    console.log("reentrancyTest Contract balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.reentrancyTestAddress)), "ETH");;
+    console.log("attacker account balance before executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attacker)), "ETH");;
 
     const amount = ethers.parseEther("1");
     const calldata = ctx.contractAttacker.interface.encodeFunctionData("call_withdraw", [amount]);
@@ -17,11 +17,11 @@ async function main () {
     const receipt = await tx.wait();
     console.log(tx.hash);
 
-    console.log("attacker account balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attackerAddress)));;
-    console.log("reentrancyTest account balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.reentrancyTestAddress)));;
-    console.log("attacker account balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attacker)));;
+    console.log("attacker Contract balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attackerAddress)), "ETH");;
+    console.log("reentrancyTest Contract balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.reentrancyTestAddress)), "ETH");;
+    console.log("attacker account balance after executing attack = ", await ethers.formatEther(await ethers.provider.getBalance(ctx.attacker)), "ETH");;
 
-    await saveTrace(tx.hash, "../../traces/hardhat_traces/attacker", "attack")
+    await saveTrace(tx.hash, "./traces/hardhat_traces/attacker", "attack");
  
 }
 main().catch(console.error);
